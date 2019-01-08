f0bdf2b6bacd0d5740b17a224817441b1bd8642a
七彩blog <35214016+lizhongnian@users.noreply.github.com>
2019-01-08 14:29:49 +0800

    初次提交

---
+++ tools4396.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# 一键脚本合集
+# 打印欢迎信息
+clear
+echo "---------------------------------------------"
+echo "  Install：欢迎使用tools4396工具"
+echo "  Author: 7colorblog"
+echo "  URL: https://www.7colorblog.com"
+echo "  Article: https://www.7colorblog.com/?id=64"
+echo "---------------------------------------------"
+echo ""
